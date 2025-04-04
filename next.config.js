52c9814d2596a7139e1d13bec685ecd321e3bc7c
Jean ROSSET <jean.rosset@yahoo.fr>
2025-04-04 18:51:25 +0100

    Désactiver ESLint et TypeScript pendant le build

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
+  experimental: {
+    // typedRoutes a été retiré car incompatible avec Turbopack
   },
   images: {
     domains: ['ouiwpkxvjxcfbypmurap.supabase.co', 'i.ibb.co']
+  },
+  eslint: {
+    // Désactiver les vérifications ESLint pendant la construction
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Désactiver les vérifications TypeScript pendant la construction
+    ignoreBuildErrors: true,
   }
 }
 
